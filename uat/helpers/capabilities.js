73aabab62fe700202995e3ebec118dc5578c85a3
rashid <rshaikh@equalexperts.com>
2015-03-10 18:12:06 +0530

    Adding iOS capabilities

--- uat/helpers/capabilities.js
+++ uat/helpers/capabilities.js
@@ -2,28 +2,28 @@
 
 'use strict';
 
-//exports.ios = {
-//  browserName: '',
-//  'appium-version': '1.1',
-//  platformName: 'iOS',
-//  platformVersion: '7.1',
-//  deviceName: 'iPhone',
-//  app: undefined // will be set later
-//};
-
-exports.android = {
+exports.ios = {
   browserName: '',
   'appium-version': '1.3.4',
-  platformName: 'Android',
-  platformVersion: '4.4.2',
-  deviceName: 'Nexus_4',
-  automationName: 'selendroid',
-  deviceType: "phone",
-  androidUseRunningApp: false,
-  appPackage: 'com.the_experimenters.hybrid_sign_in',
-  appActivity: '.BeanThere',
-  app: '/projects/appium-spike/appium-smoke-tester/sample-code/apps/android/BeanThere-debug.apk'
+  platformName: 'iOS',
+  platformVersion: '8.1',
+  deviceName: 'iPhone 6',
+  app: '../../sample-code/apps/ios/sim/BeanThere.app'
 };
+
+//exports.android = {
+//  browserName: '',
+//  'appium-version': '1.3.4',
+//  platformName: 'Android',
+//  platformVersion: '4.4.2',
+//  deviceName: 'Nexus_4',
+//  automationName: 'selendroid',
+//  deviceType: "phone",
+//  androidUseRunningApp: false,
+//  appPackage: 'com.the_experimenters.hybrid_sign_in',
+//  appActivity: '.BeanThere',
+//  app: '/projects/appium-spike/appium-smoke-tester/sample-code/apps/android/BeanThere-debug.apk'
+//};
 //
 //exports.android18 = {
 //  browserName: '',
